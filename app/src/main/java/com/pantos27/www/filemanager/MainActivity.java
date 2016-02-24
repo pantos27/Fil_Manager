4794b44a9f87cd4e1a9b9e2d76441446b7ff7370
Veierovioum <mcattar@gmail.com>
2016-02-25 00:03:48 +0200

    a  bit more headles fragment - permission handler

--- app/src/main/java/com/pantos27/www/filemanager/MainActivity.java
+++ app/src/main/java/com/pantos27/www/filemanager/MainActivity.java
@@ -1,11 +1,14 @@
 package com.pantos27.www.filemanager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.Collections;
-import java.util.concurrent.Callable;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        if (){
+
+        }
+
         FilesArray filesArray =FilesArray.getInstance();
         String[] paths=Environment.getExternalStorageDirectory().list();
 
-        
+
     }
 }
